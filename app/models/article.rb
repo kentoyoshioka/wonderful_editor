f4040ace31ac9b4e041aa9933473423eff2801a3
kentoyoshioka <brise.mer.g@gmail.com>
2020-10-16 17:38:14 +0900

    親 model が削除された際に子 model も削除されるように dependent: :destroy の設定を追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -20,5 +20,6 @@
 class Article < ApplicationRecord
   belongs_to :user
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
+  has_many :article_likes, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -38,7 +38,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
 
-  has_many :articles
-  has_many :comments
-  has_many :article_likes
+  has_many :articles, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :article_likes, dependent: :destroy
 end
