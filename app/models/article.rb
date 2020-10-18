7196e4de2e151f0d7d049a17435e27fe97dfa88c
kentoyoshioka <brise.mer.g@gmail.com>
2020-10-18 18:49:45 +0900

    各 model に validation の設定を追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -22,4 +22,7 @@ class Article < ApplicationRecord
 
   has_many :comments, dependent: :destroy
   has_many :article_likes, dependent: :destroy
+
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :bady, presence: true
 end
