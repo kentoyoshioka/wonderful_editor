f6d67bc038686e156ee7a32fc653ce51676e3ece
kentoyoshioka <brise.mer.g@gmail.com>
2020-10-22 19:12:02 +0900

    article_spec, user_spec のリファクタリング

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -20,31 +20,29 @@
 require "rails_helper"
 
 RSpec.describe Article, type: :model do
-  before do
-    @user = create(:user)
-  end
+  let(:user) { create(:user) }
 
   context "title, body, user_id カラムに値がある時"
   it "記事が登録される" do
-    article = build(:article, user_id: @user.id)
+    article = build(:article, user_id: user.id)
     expect(article).to be_valid
   end
 
   context "title カラムに値がない時"
   it "タイトルがないので記事は登録されない" do
-    article = build(:article, title: "", user_id: @user.id)
+    article = build(:article, title: "", user_id: user.id)
     expect(article).to be_invalid
   end
 
   context "title カラムの値が 20 以上の時"
   it "タイトルの文字数が 20 字以上なので記事は登録されない" do
-    article = build(:article, title: "poiuytrewqasdfghjklmnbvc", user_id: @user.id)
+    article = build(:article, title: "poiuytrewqasdfghjklmnbvc", user_id: user.id)
     expect(article).to be_invalid
   end
 
   context "body カラムに値がない時"
   it "記事の内容がないので記事は登録されない" do
-    article = build(:article, body: "", user_id: @user.id)
+    article = build(:article, body: "", user_id: user.id)
     expect(article).to be_invalid
   end
 end
