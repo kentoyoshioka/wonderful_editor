0a37c18fc611977f81c3ad86d95e51363449d819
kentoyoshioka <brise.mer.g@gmail.com>
2020-10-31 20:08:28 +0900

    Revert "article controller,article preview serializerを修正"
    This reverts commit 378ced01b1d6e8590f137758e45ddd9412d848d0.

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,6 +1,2 @@
-class Api::V1::ArticlesController < Api::V1::BaseApiController
-  def index
-    @articles = Article.all.order(updated_at: :desc)
-    render json: @articles, each_serializer: Api::V1::ArticlePreviewSerializer
-  end
+class Api::V1::ArticlesController < ApplicationController
 end
